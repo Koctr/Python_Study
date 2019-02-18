de1d4c4ce7d2e3420a2541fa9c5bf9faf1f8cdb4
Koctr <koctr@china.com>
2019-02-18 10:05:13 +0800

    add muggle_oa

---
+++ muggle_oa/christmas_tree.py
@@ -0,0 +1,12 @@
+# -*- encoding:utf-8 -*-
+# Author: Koctr
+
+
+asterisk = "*"
+v_line = "|"
+print(asterisk.center(7))
+print((3 * asterisk).center(7))
+print((5 * asterisk).center(7))
+print((7 * asterisk).center(7))
+print(v_line.center(7))
+print(v_line.center(7))
