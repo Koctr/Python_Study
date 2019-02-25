8269d761bf57fd3d751cc9d36ab2200cefec1a87
Koctr <koctr@china.com>
2019-02-25 22:03:34 +0800

    modify mysql_op.py

--- muggle_oa/mysql_op.py
+++ muggle_oa/mysql_op.py
@@ -7,7 +7,7 @@ import pymysql
 database = pymysql.connect("127.0.0.1", "root", "root", "muggle_oa", charset="utf8")
 cursor = database.cursor()
 
-sql = "select sum(weight) from data where copnamy='王五小麦' and province='河北' " \
+sql = "select sum(weight) from data where company='王五小麦' and province='河北' " \
       "and date between '2018-7-21' and '2018-7-25'"
 cursor.execute(sql)
 result = cursor.fetchall()
